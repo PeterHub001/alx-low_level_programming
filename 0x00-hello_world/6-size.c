77a85a614db54b0d260cfb3935e9ed38374306b9
Ishado Emmanuel <peter.ishado@gmail.com>
2022-10-13 22:55:11 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - Entry
+ * Return: Aways 0
+ */
+int main(void);
+{
+	printf("Size of char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %1 byte(s)\n", sizeof(int));
+	printf("Size of a long int: %1 byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %1 byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %1 byte(s)\n", sizeof(float));
+	return (0);
+
+}
