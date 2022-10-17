f6979387d3e59d67bdad69c08aa2c82740bb1a97
Ishado Emmanuel <peter.ishado@gmail.com>
2022-10-16 18:55:26 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Aways 0
+ */
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putcher(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
