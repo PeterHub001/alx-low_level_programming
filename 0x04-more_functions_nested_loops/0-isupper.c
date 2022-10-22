a29ad1d81e743e1d1b384d85dbd905842683877e
Ishado Emmanuel <peter.ishado@gmail.com>
2022-10-22 09:23:38 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: variable text
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		i = 0;
+	}
+	if (c >= 'A' && c <= 'Z')
+	{
+		i = 1;
+	}
+		return (i);
+}
